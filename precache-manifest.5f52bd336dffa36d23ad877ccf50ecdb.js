32f6f50eb56f799288d27fa8de57fb5c0c7bb501
Matheus Souza <matheu.sk.9@hotmail.com>
2020-02-16 21:08:30 -0300

    Updates

---
+++ precache-manifest.5f52bd336dffa36d23ad877ccf50ecdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bbfd8e8b0d43ebf8559e203d98aa55",
+    "url": "/impostor-front/index.html"
+  },
+  {
+    "revision": "76a0a1d8a3b610dcc53e",
+    "url": "/impostor-front/static/css/main.0efde187.chunk.css"
+  },
+  {
+    "revision": "d0cb7d2d9631115acffb",
+    "url": "/impostor-front/static/js/2.eb8c2586.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/impostor-front/static/js/2.eb8c2586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a0a1d8a3b610dcc53e",
+    "url": "/impostor-front/static/js/main.f5d3e641.chunk.js"
+  },
+  {
+    "revision": "027d78f57f749b349efb",
+    "url": "/impostor-front/static/js/runtime-main.4c2414fb.js"
+  }
+]);
